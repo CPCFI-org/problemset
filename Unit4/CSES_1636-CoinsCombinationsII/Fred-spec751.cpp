7441adb06f061f462509eaabed63b132ff1fee97
Fredin_Alberto <72536642+Fred-spec751@users.noreply.github.com>
2021-11-15 11:28:21 -0600

    Update Fred-spec751.cpp

--- Unit4/CSES_1636-CoinsCombinationsII/Fred-spec751.cpp
+++ Unit4/CSES_1636-CoinsCombinationsII/Fred-spec751.cpp
@@ -1,21 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+ 
 		//Algunos macros para ayudar
 #define endl		'\n'
-
+ 
 	//Definiciones de ciclos for: crementar, incrementar
 #define FO(i, b)	for(int i = (a); i < (b); i++)
 #define FOR(i, a, b)	for(int i = (a); i < (b); i++)
 #define rFOR(i, a, b)	for(int i = (a); i > (b); i--) //a es nuestro valor de inicio
-
+ 
 #define pint(x)			printf("%d\n",x);
 #define pll(x)			printf("%lld\n",x); //imprimir un long long
 #define si(x)			scanf("%d",&x);	    //Escaneo de un entero
 #define sl(x)			scanf("%lld",&x);
 #define TR(v, arr)      for(auto& (v) : (arr)) //Es como un for each
-
-
+ 
+ 
 				//TYPEDEF YA NO ES MACROS
 typedef vector<int> vi;
 typedef vector<long long> vll;
@@ -25,13 +25,13 @@ int main()
 	{
 	    int coins[101]={};
         
-        int monedas[20000];
+        
 		int len,sum,aux,i,j;
 		int modulo=1e9+7;
 		scanf("%d %d",&len,&sum);
 		for(int i  =0;i<len;i++)
 			{
-				scanf("%d",&coins[i]);
+				scanf("%lld",&coins[i]);
 			}
 		//sort(coins,coins+len);
 		dp[0] = 1;
@@ -43,14 +43,10 @@ int main()
 						{
 							dp[j] += dp[j-coins[i]]; 
 							dp[j]%=modulo;
-							
 						}
 						
 					}
 			}
-		
 		cout<<dp[sum];
 		return 0;
 	}
-	
-
