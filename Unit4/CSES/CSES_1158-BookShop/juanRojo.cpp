6edb827007dd9139cce02d20ec0f898fb72a41c8
Possible-99 <juanpsfi3023@gmail.com>
2021-11-22 15:52:15 -0600

    Added some solutions to DP problems

---
+++ Unit4/CSES/CSES_1158-BookShop/juanRojo.cpp
@@ -0,0 +1,145 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl            '\n'
+#define fastIO()        cin.tie(0); cout.tie(0);
+#define FO(i, b)        for (int i = 0; i < (b); i++)
+#define FOR(i, a, b)    for (int i = (a); i < (b); i++)
+#define rFOR(i, a, b)   for (int i = (a); i > (b); i--)
+#define TR(v, arr)      for(auto& (v) : (arr))
+#define debug(x)        cout << #x << " = "; _debug(x); cout << endl;
+#define si(x)           scanf("%d", &x);
+#define sl(x)           scanf("%lld", &x);
+#define pi(x)           printf("%d\n", x);
+#define pl(x)           printf("%lld\n", x);
+#define pb              push_back
+#define F               first
+#define S               second
+#define all(x)          x.begin(), x.end()
+#define sz(x)           (int) x.size()
+#define LB(arr,x)       lower_bound(all(arr),x) -(arr).begin()
+#define UB(arr,x)       upper_bound(all(arr),x) -(arr).begin()
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pii> vii;
+
+const ll MOD= 1e9 + 7;
+const ll INF= 1e9;
+
+void setIO(){
+    string file = __FILE__;
+    file = string(file.begin() , file.end() - 3);
+    string in_file = file+"in";
+    string out_file = file+"out";
+    freopen(in_file.c_str() , "r", stdin);
+    freopen(out_file.c_str(), "w",stdout);
+}
+
+template <typename T>
+void _debug(T& x){
+    cout << x;
+}
+
+template <typename T1, typename T2>
+void _debug(pair<T1,T2>& pair){
+
+    cout <<"{"; _debug(pair.F); cout << ","; _debug(pair.S); cout << "}";
+}
+
+template<typename T>
+void _debug(vector<T> & vec){
+    int n = sz(vec);
+    if(n == 0){cout << "[ ]"; return;}
+    cout << "[";
+    FO(i,n-1){
+        _debug(vec[i]); cout << " ";
+    }
+    _debug(vec[n - 1]); cout << "j";
+}
+
+void _debug(vector<string>& vec){
+    int n= sz(vec);
+    cout << endl;
+    FO(i,n){
+
+        cout << vec[i] << endl;
+    }
+}
+
+template<typename T>
+void _debug(vector<vector<T> >& A){
+    int n = sz(A);
+    cout << endl;
+    FO(i,n){
+        _debug(A[i]); cout << endl;
+    }
+}
+
+template<typename T>
+void print(T& x){
+    cout << x << endl;
+    return;
+}
+
+template<typename T>
+void print(vector<T>& vec, int a=0, int b=-1){
+    if(b == -1){b = sz(vec);}
+    if(b == 0){return;}
+    FOR(i, a, b - 1){
+        cout << vec[i] << " ";
+    }
+    cout << vec[b - 1] << endl;
+    return;
+}
+
+// -----------------------------------------------------------------------------
+// Here begins our solution
+// -----------------------------------------------------------------------------
+
+void solve() {
+   int n , x;
+   si(n); si(x);
+   vector<int> prBooks;
+   vector<int> pages;
+   vector<vector<int>> dp(n + 1 , vector<int>(x + 1 , 0));
+
+   FO(i , n){
+       int elem; si(elem);
+       prBooks.push_back(elem);
+   }
+
+
+   FO(i , n){
+       int elem; si(elem);
+       pages.push_back(elem);
+   }
+
+   for(int i = 1 ; i <= n; i++) {
+       for(int j  = 0 ; j <= x ; j++ ){
+           dp[i][j] = dp[i - 1][j];
+           if(j - prBooks[i - 1] >= 0 )
+            dp[i][j] = max(dp[i - 1][j] , dp[i - 1][j - prBooks[i - 1]] + pages[i - 1]);
+       }
+   }
+
+   cout << dp[n][x];
+
+
+}
+
+
+int main() {
+    
+    fastIO();
+    if(getenv("LOCAL")){setIO();}
+    int T = 1;
+    FO(tc, T){
+        solve();
+    }
+    return 0;
+}
