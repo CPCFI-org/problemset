556d92cf4b31401a022fd23d8f22855e1fa3f93a
Navarrete <aaldiitoo@gmail.com>
2022-02-15 16:44:17 -0600

    CSES 1671 Solutions Folder

---
+++ Unit6/Graph_Traversal_I/CSES/CSES_1671_ShortestRoutesI/aldonavarretefp.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define PD(t) printf("%d",t)
+#define PLD(t) printf("%ld",t)
+#define PLLD(t) printf("%lld",t)
+#define PC(t) printf("%c",c)
+#define PS(t) printf("%s",t)
+#define PF(t) printf("%f",t)
+#define PLF(t) printf("%lf",t)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define FO(i, b) for(int  i = 0; i<b; i++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define EPS 1e-9
+#define PI acos(-1.0)
+#define MOD 1000000007
+#define read(type) readInt<type>()
+#define TR(v,arr) for (auto& (v): (arr))
+#define endl '\n'
+#define fastIO cin.tie(0); cout.tie(0);
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef queue<int> QI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+#define int long long int
+
+const int MAXD = 1e17;
+vector<vector<pair<int,int> > > g;
+vector<int> dist;
+
+void solve(){
+    //SSSP
+    int n,m;
+    cin>>n>>m;
+    g.resize(n+1);
+    dist.resize(n+1,MAXD);
+    for(int i = 0 ; i < n; i++){
+        dist[i] = MAXD;
+    }
+
+	for(int  i = 0; i < m; ++i){
+        int u, v, c;
+		cin >> u >> v >> c;
+		g[u].push_back(MP(v,c));
+	}
+    priority_queue<pair<int,int>,
+    vector<pair<int,int> >,
+    greater<pair<int,int> > > pq;
+    dist[1] = 0;
+    pq.push(MP(0,1));
+    while(!pq.empty()){
+
+        int u = pq.top().second;
+        int d = pq.top().first;
+        pq.pop();
+
+        if(dist[u] < d) continue;
+        else{
+            for(auto e : g[u]){
+                int v = e.first;
+                int c = e.second;
+                if(dist[v] <= c+d) continue;
+                else{
+                    dist[v] = c+d;
+                    pq.push(MP(dist[v],v));
+                }
+
+            }
+        }
+    }
+    debug(dist);
+    for(int i = 1; i <= n; ++i){
+        cout<<dist[i]<<" ";
+    }
+}
+
+void setIO(){
+    string file = __FILE__;
+    file = string(file.begin(),file.end()-3);
+    string input_file = file + "in";
+    string output_file = file + "out";
+    freopen(input_file.c_str(), "r",stdin);
+    freopen(output_file.c_str(),"w",stdout);
+
+}
+
+/********** Main()  function **********/
+int32_t main()
+{
+    
+    if(getenv("CP_IO")){setIO();}
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int T=1;
+    FO(tc,T)
+        solve();
+    return 0;
+}
